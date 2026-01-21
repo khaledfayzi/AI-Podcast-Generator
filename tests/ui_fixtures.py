0b42e3dabbdb4fc554a9a7fb5774179414c657ea
Mustafa Canbakan <mustafa.canbakan@smail.th-koeln.de>
2026-01-21 01:05:29 +0100

    final fix

--- tests/ui_fixtures.py
+++ tests/ui_fixtures.py
@@ -92,13 +92,15 @@ def gradio_server(mock_controller):
 
 @pytest.fixture(scope="function")
 def driver():
-    """
-    Setup Selenium WebDriver.
-    """
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")  # Uncomment to run invisible
+
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
-    
+    # chrome_options.add_argument("--headless")  # Uncomment to run invisible
+
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
